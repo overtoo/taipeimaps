317c9b91af9ab942eee7b41271bbeb42b4657640
overtoo <s_bajo@hotmail.com>
2021-12-21 23:38:19 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -50,13 +50,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by 𝖄𝖔𝖚𝖗 𝕯𝖆𝖉'𝖘 𝕬𝖘𝖘
-        </a>
+        <a rel="noopener noreferrer">Powered by 𝖄𝖔𝖚𝖗 𝕯𝖆𝖉'𝖘 𝕬𝖘𝖘</a>
       </footer>
     </div>
   );
